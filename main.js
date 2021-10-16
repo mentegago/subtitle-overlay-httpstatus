e9ab94a54200feddf7d6b1d2cdf88918a68dc341
Mentega <mentegago@gmail.com>
2021-10-17 01:21:07 +0800

    Generalize functionality to allow other subtitles

--- main.js
+++ main.js
@@ -8,20 +8,11 @@ const showSubtitle = (subtitles, index = -1, initialStartOffset = 0) => {
     if(index == -1) {
         const nextStartTime = subtitles[index+1].startTime
         subtitleTimerId = setTimeout(() => { showSubtitle(subtitles, index+1) }, nextStartTime + initialStartOffset)
-        subtitleContainer.innerHTML = "<span class=\"sub_status\">🧈 Mentega Subtitle Overlay 🧈</span>"
+        subtitleContainer.innerHTML = "<span class=\"sub_status\">🧈 Subtitle starting... 🧈</span>"
         return;
     }
 
-    const songName = (i) => {
-        if(i<8) return "Sik Sik Sibatumanikam (North Sumatra)";
-        if(i<16) return "Kicir Kicir (DKI Jakarta)";
-        if(i<28) return "Cublak Cublak Suweng (Central Java)";
-        if(i<34) return "Bungong Jeumpa (Aceh)";
-        if(i<38) return "Apuse (Papua)"
-        return "";
-    };
-
-    subtitleContainer.innerHTML = `<span class="song_name">${songName(index)}</span><br>${subtitles[index].text}`;
+    subtitleContainer.innerHTML = subtitles[index].text;
     if(index+1 == subtitles.length) return;
 
     const currentStartTime = subtitles[index].startTime;
@@ -35,7 +26,7 @@ const connect = (url, subtitles) => {
     ws.onmessage = (message) => {
         const msg = JSON.parse(message.data)
         if(msg.event == "songStart") {
-            if(msg.status.beatmap.songName.includes("Indonesia")) {
+            if(!songName || msg.status.beatmap.songName.toLowerCase().includes(songName)) {
                 if(subtitleTimerId) {
                     clearTimeout(subtitleTimerId)
                     subtitleTimerId = null
@@ -43,7 +34,7 @@ const connect = (url, subtitles) => {
 
                 isSongRunning = true
                 subtitleContainer.style.display = 'block'
-                showSubtitle(subtitles, -1, 1924)
+                showSubtitle(subtitles, -1, initialDelay)
             }
         } else if(msg.event == "menu") {
             if(subtitleTimerId) {
@@ -53,7 +44,7 @@ const connect = (url, subtitles) => {
 
             if(isSongRunning) {
                 isSongRunning = false
-                subtitleContainer.innerHTML = "<span class=\"sub_status\">🧈 Mentega Subtitle Overlay Stopped 🧈</span>"
+                subtitleContainer.innerHTML = "<span class=\"sub_status\">🧈 Subtitle stopped 🧈</span>"
                 setTimeout(() => { 
                     subtitleContainer.innerHTML = ''
                     subtitleContainer.style.display = 'none'
@@ -77,7 +68,14 @@ const connect = (url, subtitles) => {
     }
 }
 
-loadSubtitle("https://gist.githubusercontent.com/mentegago/9ed07208bbcc71e6802b1e4422b17d7c/raw/5851d3726fcdc183719a4f4ff58ee52f21b138e8/subtitle.srt")
+const urlSearchParams = new URLSearchParams(window.location.search)
+const subtitleURL = urlSearchParams.get('url')
+const songName = urlSearchParams.has('name') ? urlSearchParams.get('name').toLowerCase() : null
+const initialDelay = urlSearchParams.has('initialdelay') ? parseInt(urlSearchParams.get('initialdelay')) : 0
+
+// https://gist.githubusercontent.com/mentegago/9ed07208bbcc71e6802b1e4422b17d7c/raw/5851d3726fcdc183719a4f4ff58ee52f21b138e8/subtitle.srt
+// delay: 1924
+loadSubtitle(subtitleURL)
     .then(subtitles => {
         console.log("Subtitle loaded! Starting websocket...")
         connect('ws://127.0.0.1:6557/socket', subtitles)
